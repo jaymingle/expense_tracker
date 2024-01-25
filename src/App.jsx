f18fbdb88a1ec8fc777de7bf0c87fad16ec9c472
Jay Mingle <jkawudi@gmail.com>
2024-01-25 21:19:38 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from "./components/Header.jsx";
+import Balance from "./components/Balance.jsx";
 
 function App() {
   return (
     <>
         <Header/>
         <div className="container">
-
+            <Balance/>
         </div>
     </>
   )
