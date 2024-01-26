33c02d825d0743814caf80187865da7fe9c25cf5
Jay Mingle <jkawudi@gmail.com>
2024-01-26 12:51:57 +0000

    Changes

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,9 +1,9 @@
 const Balance = () => {
     return(
-        <div>
+        <>
             <h4>Your Balance</h4>
             <h1 className="balance">$0.00</h1>
-        </div>
+        </>
     )
 }
 export default Balance;
