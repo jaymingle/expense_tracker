ff9b11f161710cf19af5b03e91ee373482b5888f
Jay Mingle <jkawudi@gmail.com>
2024-01-27 23:49:49 +0000

    Changes

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
+import {GlobalContext} from "../context/GlobalState.jsx";
 
 const Transaction = ({id, amount, text}) => {
-
+    const context = useContext(GlobalContext);
     const sign = amount < 0 ? '-' : '+';
 
     const deleteTransactionHandler = () => {
         console.log(id, text)
+        console.log(context)
     }
 
     return (
