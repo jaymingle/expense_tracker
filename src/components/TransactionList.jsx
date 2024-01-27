c073d502b743497b13c10bc0455e400177cc5b06
Jay Mingle <jkawudi@gmail.com>
2024-01-27 14:39:46 +0000

    Changes

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -12,7 +12,7 @@ const TransactionList = () => {
             <h3>History</h3>
             <ul className="list">
                 {transactions.map(transaction =>
-                    <li className="minus" key={transaction.id}>
+                    <li className={`${transaction.amount > 0 ? 'plus' : 'minus' }`} key={transaction.id}>
                         {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
                     </li>
                 )}
