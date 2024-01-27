b9d18853beca18c01424337fe1bfe4d1bf064005
Jay Mingle <jkawudi@gmail.com>
2024-01-27 22:02:58 +0000

    Changes

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Transaction = ({amount, text}) => {
+
+    const sign = amount < 0 ? '-' : '+';
+
     return (
         <div>
             <li className={`${amount > 0 ? 'plus' : 'minus' }`}>
-                {text} <span>{amount}</span><button className="delete-btn">x</button>
+                {text} <span>{sign}${amount}</span><button className="delete-btn">x</button>
             </li>
         </div>
     )
