648930256c278f8f5845cbdd5a6621264f510327
Jay Mingle <jkawudi@gmail.com>
2024-01-27 11:46:16 +0000

    Changes

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -4,7 +4,7 @@ const TransactionList = () => {
             <h3>History</h3>
             <ul id="list" className="list">
                 <li className="minus">
-                Cash <span>-$400</span><button className="delete-btn">x</button>
+                     Cash <span>-$400</span><button className="delete-btn">x</button>
                 </li>
             </ul>
         </>
