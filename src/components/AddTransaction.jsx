3cf792edbf025d4fefd736ff908831a3d07d2411
Jay Mingle <jkawudi@gmail.com>
2024-01-27 11:55:52 +0000

    Changes

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -15,10 +15,7 @@ const AddTransaction = () => {
                     <input type="text" id="text" placeholder="Enter text..."/>
                 </div>
                 <div className="form-control">
-                    <label htmlFor="amount"
-                    >Amount <br/>
-                        (negative - expense, positive - income)</label
-                    >
+                    <label htmlFor="amount">Amount <br/>(negative - expense, positive - income)</label>
                     <input type="number" id="amount" placeholder="Enter amount..."/>
                 </div>
                 <input className="btn" type="submit">Add transaction</input>
