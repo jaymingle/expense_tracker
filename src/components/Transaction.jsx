7ce37179abac13acb760aeb4c3d5b9bfc0606030
Jay Mingle <jkawudi@gmail.com>
2024-01-27 14:54:45 +0000

    Changes

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,7 +1,11 @@
-const Transaction = () => {
+import React from "react";
+
+const Transaction = ({id, amount, text}) => {
     return (
         <div>
-
+            <li className={`${transaction.amount > 0 ? 'plus' : 'minus' }`} key={transaction.id}>
+                {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
+            </li>
         </div>
     )
 }
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -12,9 +12,7 @@ const TransactionList = () => {
             <h3>History</h3>
             <ul className="list">
                 {transactions.map(transaction =>
-                    <li className={`${transaction.amount > 0 ? 'plus' : 'minus' }`} key={transaction.id}>
-                        {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
-                    </li>
+
                 )}
             </ul>
         </>
