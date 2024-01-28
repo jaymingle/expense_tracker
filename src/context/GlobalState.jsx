b5f4c39db72f89eebe249ee0344fb62ec6dc639a
Jay Mingle <jkawudi@gmail.com>
2024-01-28 12:08:23 +0000

    Changes

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -18,6 +18,15 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    //Actions
+    function deleteTransactions(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
+
     return (<GlobalContext.Provider value={{transactions: state.transactions}}>
         {children}
     </GlobalContext.Provider>)
