7c40c31c26fb030546a959488cbbe15a17dddde3
Jay Mingle <jkawudi@gmail.com>
2024-01-28 12:03:28 +0000

    Changes

--- src/components/IncomeExpense.jsx
+++ src/components/IncomeExpense.jsx
@@ -9,7 +9,7 @@ const IncomeExpense = () => {
     const income = Number(amount.filter(figure => figure > 0).reduce((income, total) => total += income,0).toFixed(2))
     console.log(income)
 
-    const expense = Number(amount.filter(figure => figure < 0).reduce((expense, total) => total += expense, 0).toFixed(2))
+    const expense = Number((amount.filter(figure => figure < 0).reduce((expense, total) => total += expense, 0) * -1).toFixed(2))
     console.log(expense)
 
     return(
