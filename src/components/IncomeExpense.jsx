e39dfffecc1326a6a72bf19436e0e7e675c5d847
Jay Mingle <jkawudi@gmail.com>
2024-01-28 11:53:33 +0000

    Changes

--- src/components/IncomeExpense.jsx
+++ src/components/IncomeExpense.jsx
@@ -1,4 +1,11 @@
+import React, {useContext} from "react";
+import {GlobalContext} from "../context/GlobalState.jsx";
+
 const IncomeExpense = () => {
+
+    const {transactions} = useContext(GlobalContext)
+   console.log(transactions)
+
     return(
         <div className="inc-exp-container">
             <div>
