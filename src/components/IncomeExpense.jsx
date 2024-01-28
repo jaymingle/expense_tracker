51cbcf58e513a1372baa2873c2fa8e0f8f9aa71b
Jay Mingle <jkawudi@gmail.com>
2024-01-28 11:58:17 +0000

    Changes

--- src/components/IncomeExpense.jsx
+++ src/components/IncomeExpense.jsx
@@ -8,17 +8,17 @@ const IncomeExpense = () => {
    // console.log(transactions)
     const amount = transactions.map(transaction => transaction.amount)
     // console.log(amount)
-    const income = amount.filter(figure => figure > 0).reduce((income, total) => total += income,0)
+    const income = amount.filter(figure => figure > 0).reduce((income, total) => total += income,0).toFixed(2)
     console.log(income)
 
-    const expense = amount.filter(figure => figure < 0).reduce((expense, total) => total += expense, 0)
+    const expense = amount.filter(figure => figure < 0).reduce((expense, total) => total += expense, 0).toFixed(2)
     console.log(expense)
 
     return(
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
+                <p className="money plus">+${0.0}0</p>
             </div>
             <div>
                 <h4>Expense</h4>
